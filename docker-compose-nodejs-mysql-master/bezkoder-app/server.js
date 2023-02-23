5c4084e63f50611ec5dddca566da7fa913543f47
Ainsley <120529093+MrSquishmeister@users.noreply.github.com>
2023-02-23 13:38:54 +0000

    docker feat: mysql and nodejs example

--- docker-compose-nodejs-mysql-master/bezkoder-app/server.js
+++ docker-compose-nodejs-mysql-master/bezkoder-app/server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 
@@ -15,13 +16,23 @@ app.use(express.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 
+const db = require("./app/models");
+
+db.sequelize.sync();
+// // drop the table if it already exists
+// db.sequelize.sync({ force: true }).then(() => {
+//   console.log("Drop and re-sync db.");
+// });
+
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to the application." });
+  res.json({ message: "Welcome to bezkoder application." });
 });
 
+require("./app/routes/turorial.routes")(app);
+
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.NODE_DOCKER_PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
